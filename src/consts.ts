9ab9a6b43491aacfaaf1e0f8dd990f279599bd18
Mike McMahon <mike.mcmahon67@gmail.com>
2026-04-01 20:28:14 -0600

    update footer.astro and consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'Mike McMahon';
-export const SITE_DESCRIPTION = 'Infrastructure engineer, AI practitioner, homelab builder. Documenting the work.';
+export const SITE_DESCRIPTION = 'Infrastructure engineer, AI practitioner, Homelab builder, Dad-joke Deliveryman. Documenting the work.';
